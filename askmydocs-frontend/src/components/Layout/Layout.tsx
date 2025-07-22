9fa83a02fc76ec4a5fa85c63e82a8cb8c0d67094
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-07-21 21:46:17 -0300

    feat: Cria componente de layout

---
+++ askmydocs-frontend/src/components/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from "react";
+import Sidebar from "../Sidebar/Sidebar";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="w-full min-h-screen bg-base-100 flex">
+      <Sidebar files={[
+        "relatorio-financeiro-jan.pdf",
+        "relatorio-financeiro-fev.pdf",
+        "relatorio-financeiro-mar.pdf",
+        "relatorio-financeiro-abr.pdf",
+        "relatorio-financeiro-mai.pdf"
+      ]} />
+      <main className="flex-1 p-4">{children}</main>
+    </div>
+  );
+}
